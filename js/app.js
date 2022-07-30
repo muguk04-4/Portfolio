25aedebbcc3e9d3d25b2eb66715d2d5c05287334
DESKTOP-LOSKL2P\A <garuru7080@gmail.com>
2022-07-30 17:54:21 +0900

    data prepare

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@
 // 객체만들어야됨(이미지만 배열로)
 // 여기다 만들어도 되지만 차라리 컨트롤러쪽에 만들어도 되지 않을까
 // 고민중
+const webData = [{
+  workName: '로스트아크 길드매니저',
+  workImg: ['img/work_img/web/lostark_guildmanager1.jpg'],
+  workInfo: '이것은 로아 길드매니저',
+ 
+}, {
+  workName: '워프레임 정보 페이지',
+  workImg: ['img/work_img/web/warframe1.jpg', 'img/work_img/web/warframe2.jpg', 'img/work_img/web/warframe3.jpg'],
+  workInfo: '이것은 워프레임 페이지',
+}];
 
 // Controller ------------------------
 // C: Change works
@@ -35,8 +45,7 @@ const imgChangeLeft = () => {
 // 기본은 웹으로 되어있고 다른 분야 클릭시 
 // 그 분야 true. 다른 분야 false.
 // true가 된 분야만 보여주기.
-// 내용이 더 많으면 일단 for문 처리
-// (workName으로 구별 할수 있다면 시도)
+// workName 이름 중복 바꿔줘야함!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const workRender = () =>{
   const works = document.getElementById('works');
 
@@ -58,7 +67,7 @@ const workRender = () =>{
   workImg.appendChild(rightChange);
   const img = document.createElement('img');
   img.id = 'workImg';
-  img.src = 'img/work_img/woork.png';
+  img.src = 'img/work_img/web/lostark_guildmanager1.jpg';
   workImg.appendChild(img);
   works.appendChild(workImg);
 
